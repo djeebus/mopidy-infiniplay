d0ba35038c2156c6cb9663b09be2e8deaa0675e1
Joe Lombrozo <joe@djeebus.net>
2018-10-07 17:05:56 -0400

    Monitor and rebuild tracks in the background

--- mopidy_infiniplay/ext.py
+++ mopidy_infiniplay/ext.py
@@ -2,6 +2,8 @@ import logging
 import pkg_resources
 import pykka
 import random
+import threading
+import time
 
 from mopidy.audio.constants import PlaybackState
 from mopidy.config import Integer
@@ -12,6 +14,7 @@ from mopidy.ext import Extension
 from mopidy_infiniplay import __version__
 
 logger = logging.getLogger('mopidy-infiniplay')
+CHECK_STATE = 'BUILD_TRACKLIST'
 
 
 class InfiniPlayController(pykka.ThreadingActor, CoreListener):
@@ -24,12 +27,28 @@ class InfiniPlayController(pykka.ThreadingActor, CoreListener):
         self.min_tracks = config['infiniplay']['min_tracks']
         self.core = core  # type: Core
 
+        self._running = True
         self._tracklist = None
+        self._nanny = threading.Thread(target=self._run_nanny)
+
+    def _run_nanny(self):
+        while self._running:
+            self._add_tracks()
+            time.sleep(1)
 
     def on_start(self):
+        print('start!')
+        self._running = True
+        self._nanny.start()
+
         self._check_state()
         self._configure_mopidy()
         self._build_tracklist()
+
+    def on_stop(self):
+        print('stop!')
+        self._running = False
+        self._nanny.join()
 
     def playback_state_changed(self, old_state, new_state):
         self._check_state()
@@ -46,6 +65,14 @@ class InfiniPlayController(pykka.ThreadingActor, CoreListener):
 
     def _check_state(self):
         playback = self.core.playback
+
+        self._add_tracks()
+
+        state = playback.get_state().get()
+        if state == PlaybackState.STOPPED:
+            playback.play()
+
+    def _add_tracks(self):
         tracklist = self.core.tracklist
 
         if self._tracklist is None:
@@ -61,14 +88,15 @@ class InfiniPlayController(pykka.ThreadingActor, CoreListener):
 
             track_uri = selector()
             if not track_uri:
-                logger.warning("Could not find tracks to add")
-                return
+                logger.warning("Could not find tracks to add, sleeping")
+                if not length:
+                    # no tracks exist, let the nanny pick it up later
+                    return
+
+                # some tracks exist, make sure playback starts
+                break
 
             tracklist.add(uris=[track_uri]).get()
-
-        state = playback.get_state().get()
-        if state == PlaybackState.STOPPED:
-            playback.play()
 
     def _get_track_from_cache(self):
         return random.choice(self._tracklist)
@@ -77,7 +105,7 @@ class InfiniPlayController(pykka.ThreadingActor, CoreListener):
         items = self.core.library.browse(url).get()
         random.shuffle(items)
 
-        while True:
+        while items:
             item = items.pop()
             uri = item.uri
 
@@ -127,6 +155,7 @@ class InfiniPlayController(pykka.ThreadingActor, CoreListener):
             completed_work.add(uri)
 
         self._tracklist = tracklist
+        logger.info('done configurating')
 
 
 class InfiniPlayExtension(Extension):
